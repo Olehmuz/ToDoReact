5378ffd5f586362f5afe644f605cb28dd27ba273
Olehmuz <olehmuz87@gmail.com>
2022-05-19 15:51:11 +0300

    sidebar fix

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,11 +1,12 @@
 import "./list.scss";
+import classNames from "classnames";
 const List = ({items, isRemovable}) => {
 
     return (
         <ul className="sidebar__list">
             {items.map((el, ind) =>{
                 return (
-                    <li key={el+ind} className={el.active ? "sidebar__active" : ""}>
+                    <li key={el+ind} className={classNames(el.className, {sidebar__active:el.active})}>
                         <i>{el.icon ? el.icon : <i className={`sidebar__icon sidebar__icon--${el.color}`}></i>}</i>
                         <span>{el.name}</span>
                     </li>
